66567d6a64cded1c8b647bf55211fd675e6e5132
thaynnaraa <thaynnara_sa@hotmail.com>
2026-06-17 16:59:06 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,42 +1,57 @@
 idade_usuario = input("Por favor, insira sua idade: ")
 
-print("Agora, seja bem-vindo(a) à nossa calculadora!")
+print("Agora sim, seja bem-vindo(a) à nossa calculadora!")
 
 print("Que tal praticar um pouco de matemática básica? Vamos lá!")
 
-print("Escolha qual das operações você deseja realizar:")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Potenciação")
-print("5 - Divisão")
-print("6 - Sair")
+while True:
+  print("Escolha qual das operações você deseja realizar:")
+  print("\n=== MENU ===")
+  print("1 - Soma")
+  print("2 - Subtração")
+  print("3 - Multiplicação")
+  print("4 - Potenciação")
+  print("5 - Divisão")
+  print("6 - Sair")
 
-opcao = input("Digite o número da operação escolhida: ")
+  opcao = input("Digite o número da operação escolhida: ")
 
-num1 = int(input("Por favor, digite o primeiro número que vamos utilizar: "))
-num2 = int(input("Agora digite o segundo: "))
+  if opcao == "6":
+    print("Até logo! Encerramos por aqui.")
+    break
 
-if opcao == "1":
-  resultado = num1 + num2
-  print("Resultado:", resultado)
-elif opcao == "2":
-  resultado = num1 - num2
-  print("Resultado:", resultado)
-elif opcao == "3":
-  resultado = num1 * num2
-  print("Resultado:", resultado)
-elif opcao == "4":
-  resultado = num1 ** num2
-  print("Resultado:", resultado)
-elif opcao == "5":
-  if num2 == 0:
-     print("A divisão por zero não é possível.")
-  else:
-     resultado = num1 / num2
-     print("Resultado:", resultado)
-elif opcao == "6":
-  print("Até logo. Encerramos por aqui.")
+  if opcao not in ["1", "2", "3", "4", "5"]:
+    print("Opção inválida!")
+    continue
 
-else:
-  print("Opção inválida!") 
+  try:
+    num1 = int(input("Por favor, digite o primeiro número que vamos utilizar: "))
+    num2 = int(input("Agora digite o segundo: "))
+  except ValueError:
+    print("Por favor, digite apenas números!")
+    continue
+
+  if opcao == "1":
+    resultado = num1 + num2
+    print("Resultado:", resultado)
+  elif opcao == "2":
+    resultado = num1 - num2
+    print("Resultado:", resultado)
+  elif opcao == "3":
+    resultado = num1 * num2
+    print("Resultado:", resultado)
+  elif opcao == "4":
+    resultado = num1 ** num2
+    print("Resultado:", resultado)
+  elif opcao == "5":
+    if num2 == 0:
+      print("A divisão por zero não é possível.")
+    else:
+      resultado = num1 / num2
+      print("Resultado:", resultado)
+
+   continuar = input("\nDeseja realizar outra operação? (s/n): ")
+
+   if continuar.lower() != "s":
+     print("Até logo!")
+     break
